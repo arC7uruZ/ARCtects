31bc1033a7992d3dd6a049b714bfa00466097586
Welton <welton.carvalho.dev@gmail.com>
2026-03-31 08:33:46 -0300

    feat: configure base URL for build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+    base: '/ARCtects/',
+	plugins: [tailwindcss(), sveltekit()],
 });
